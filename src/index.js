d73986710128fe6a427e4a447ab1401ce58f023d
kuhvehl <96754228+kuhvehl@users.noreply.github.com>
2024-08-02 13:00:30 -0400

    Redo home page using JS
    Create main homepage content using JavaScript only.
    Write initial page-load function inside of its own
    module and then import and call it inside of
    index.js.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 import './style.css';
 import paperBg from './paper-bg.jpg';
+import tablecloth from './tableclothPhoto.jpg';
+import barcode from './barcode.png';
+import { homepageDisplay } from './homepage';
+
+homepageDisplay();
