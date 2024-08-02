f4061c72ede6576f606ef1f20f170f1703aa4f32
kuhvehl <96754228+kuhvehl@users.noreply.github.com>
2024-08-02 15:52:44 -0400

    Update addListeners timing

--- src/index.js
+++ src/index.js
@@ -3,23 +3,19 @@ import { homepageDisplay } from './homepage';
 import { contactDisplay } from './contact';
 import { menuDisplay } from './menu';
 
+let nav = document.querySelectorAll('button');
 
-
+function addListeners() {
+    nav.forEach(button => {
+        button.addEventListener('click', function(e) {
+            const page = e.target.getAttribute('class');
+            loadPage(page);
+        } )  
+    });
+}
 
 function loadPage(page) {
     document.querySelector('#content').innerHTML = '';
-
-    let nav = document.querySelectorAll('button');
-
-    function addListeners() {
-        nav.forEach(button => {
-            button.addEventListener('click', function(e) {
-                const page = e.target.getAttribute('class');
-                loadPage(page);
-            } )  
-        });
-    }
-
 
     if (page === 'contact') {
         contactDisplay();
